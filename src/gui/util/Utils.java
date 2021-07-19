313d0bd46615b2496e5280fe29bc87eb215fc07b
Raphael Nacarato <nacarato.raphael@gmail.com>
2021-07-19 10:02:00 -0300

    Saving Seller

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -102,4 +102,13 @@ public class Utils {
 			}
 		});
 	}
+
+	public static Double tryParseToDouble(String str) {
+		try {
+			return Double.parseDouble(str);
+
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
 }
